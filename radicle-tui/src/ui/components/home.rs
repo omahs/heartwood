5c8d83fac5a384d00bc20284914d75b263f5b22c
Erik Kundt <erik@zirkular.io>
2023-05-17 12:28:42 +0200

    tui: Use table model in patch browser

--- radicle-tui/src/ui/components/home.rs
+++ radicle-tui/src/ui/components/home.rs
@@ -1,14 +1,24 @@
+use radicle::cob::patch::Patches;
+use radicle::Profile;
+
+use radicle::storage::ReadStorage;
+
+use radicle::prelude::Id;
 use tuirealm::command::{Cmd, CmdResult};
 use tuirealm::props::Props;
 use tuirealm::tui::layout::Rect;
 use tuirealm::{AttrValue, Attribute, Frame, MockComponent, State};
 
+use crate::ui::cob::PatchItem;
 use crate::ui::layout;
+use crate::ui::theme::Theme;
 use crate::ui::widget::{Widget, WidgetComponent};
 
 use super::common::container::LabeledContainer;
 use super::common::context::Shortcuts;
 use super::common::label::Label;
+use super::common::list::{ColumnWidth, Table, TableModel};
+use super::*;
 
 pub struct Dashboard {
     about: Widget<LabeledContainer>,
@@ -79,5 +89,82 @@ impl WidgetComponent for IssueBrowser {
 
     fn perform(&mut self, _properties: &Props, _cmd: Cmd) -> CmdResult {
         CmdResult::None
+    }
+}
+
+pub struct PatchBrowser {
+    table: Widget<Table<PatchItem, 8>>,
+    shortcuts: Widget<Shortcuts>,
+}
+
+impl PatchBrowser {
+    pub fn new(theme: &Theme, profile: &Profile, id: &Id, shortcuts: Widget<Shortcuts>) -> Self {
+        let repo = profile.storage.repository(*id).unwrap();
+        let patches = Patches::open(&repo).unwrap();
+
+        let mut model = TableModel::new(
+            [
+                label(" ● "),
+                label("ID"),
+                label("Title"),
+                label("Author"),
+                label("Head"),
+                label("+"),
+                label("-"),
+                label("Updated"),
+            ],
+            [
+                ColumnWidth::Fixed(3),
+                ColumnWidth::Fixed(7),
+                ColumnWidth::Grow,
+                ColumnWidth::Fixed(21),
+                ColumnWidth::Fixed(7),
+                ColumnWidth::Fixed(4),
+                ColumnWidth::Fixed(4),
+                ColumnWidth::Fixed(18),
+            ],
+        );
+
+        if let Ok(all) = patches.all() {
+            let mut patches = all.flatten().collect::<Vec<_>>();
+            patches.sort_by(|(_, a, _), (_, b, _)| b.timestamp().cmp(&a.timestamp()));
+            patches.sort_by(|(_, a, _), (_, b, _)| a.state().cmp(b.state()));
+
+            for (id, patch, _) in patches {
+                if let Ok(item) = PatchItem::try_from((profile, &repo, id, patch)) {
+                    model.push_item(item);
+                }
+            }
+        }
+
+        let table = Widget::new(Table::new(model, theme.clone(), 2))
+            .highlight(theme.colors.item_list_highlighted_bg);
+        Self { table, shortcuts }
+    }
+
+    pub fn selected_item(&self) -> Option<&PatchItem> {
+        self.table.selection()
+    }
+}
+
+impl WidgetComponent for PatchBrowser {
+    fn view(&mut self, _properties: &Props, frame: &mut Frame, area: Rect) {
+        let shortcuts_h = self
+            .shortcuts
+            .query(Attribute::Height)
+            .unwrap_or(AttrValue::Size(0))
+            .unwrap_size();
+        let layout = layout::root_component(area, shortcuts_h);
+
+        self.table.view(frame, layout[0]);
+        self.shortcuts.view(frame, layout[1]);
+    }
+
+    fn state(&self) -> State {
+        self.table.state()
+    }
+
+    fn perform(&mut self, _properties: &Props, cmd: Cmd) -> CmdResult {
+        self.table.perform(cmd)
     }
 }
--- radicle-tui/src/ui/widget/common.rs
+++ radicle-tui/src/ui/widget/common.rs
@@ -1,14 +1,15 @@
-use radicle::Profile;
-use tuirealm::props::{AttrValue, Attribute, PropPayload, PropValue, TextSpan};
+use tuirealm::props::Attribute;
 use tuirealm::MockComponent;
 
 use crate::ui;
-use crate::ui::components::common::container::Header;
+use crate::ui::components::common::list::ColumnWidth;
 
-use ui::components::common::container::{GlobalListener, LabeledContainer, Tabs};
+use ui::components;
+
+use ui::components::common::container::{GlobalListener, Header, LabeledContainer, Tabs};
 use ui::components::common::context::{Shortcut, Shortcuts};
 use ui::components::common::label::Label;
-use ui::components::common::list::{List, Property, PropertyList, Table};
+use ui::components::common::list::{Property, PropertyList, TableModel};
 use ui::theme::Theme;
 
 use super::Widget;
@@ -17,46 +18,10 @@ pub fn global_listener() -> Widget<GlobalListener> {
     Widget::new(GlobalListener::default())
 }
 
-pub fn label(content: &str) -> Widget<Label> {
-    // TODO: Remove when size constraints are implemented
-    let width = content.chars().count() as u16;
+pub fn container_header(theme: &Theme, label: Widget<Label>) -> Widget<Header<(), 1>> {
+    let model = TableModel::new([label], [ColumnWidth::Fixed(100)]);
 
-    Widget::new(Label::default())
-        .content(AttrValue::String(content.to_string()))
-        .height(1)
-        .width(width)
-}
-
-pub fn reversable_label(content: &str) -> Widget<Label> {
-    let content = &format!(" {content} ");
-    label(content)
-}
-
-pub fn container_header(theme: &Theme, label: &str) -> Widget<Header> {
-    let content = AttrValue::Payload(PropPayload::Vec(vec![PropValue::TextSpan(
-        TextSpan::from(&format!(" {label} ")).fg(theme.colors.default_fg),
-    )]));
-    let widths = AttrValue::Payload(PropPayload::Vec(vec![PropValue::U16(100)]));
-
-    Widget::new(Header::default())
-        .content(content)
-        .custom("widths", widths)
-}
-
-pub fn table_header(theme: &Theme, labels: &[&str], widths: &[u16]) -> Widget<Header> {
-    let content = labels
-        .iter()
-        .map(|label| {
-            PropValue::TextSpan(TextSpan::from(label.to_string()).fg(theme.colors.default_fg))
-        })
-        .collect::<Vec<_>>();
-    let widths = AttrValue::Payload(PropPayload::Vec(
-        widths.iter().map(|w| PropValue::U16(*w)).collect(),
-    ));
-
-    Widget::new(Header::default())
-        .content(AttrValue::Payload(PropPayload::Vec(content)))
-        .custom("widths", widths)
+    Widget::new(Header::new(model, theme.clone(), 0))
 }
 
 pub fn labeled_container(
@@ -64,16 +29,19 @@ pub fn labeled_container(
     title: &str,
     component: Box<dyn MockComponent>,
 ) -> Widget<LabeledContainer> {
-    let header = container_header(theme, title);
+    let header = container_header(
+        theme,
+        components::label(&format!(" {title} ")).foreground(theme.colors.default_fg),
+    );
     let container = LabeledContainer::new(header, component);
 
     Widget::new(container)
 }
 
 pub fn shortcut(theme: &Theme, short: &str, long: &str) -> Widget<Shortcut> {
-    let short = label(short).foreground(theme.colors.shortcut_short_fg);
-    let divider = label(&theme.icons.whitespace.to_string());
-    let long = label(long).foreground(theme.colors.shortcut_long_fg);
+    let short = components::label(short).foreground(theme.colors.shortcut_short_fg);
+    let divider = components::label(&theme.icons.whitespace.to_string());
+    let long = components::label(long).foreground(theme.colors.shortcut_long_fg);
 
     // TODO: Remove when size constraints are implemented
     let short_w = short.query(Attribute::Width).unwrap().unwrap_size();
@@ -87,7 +55,7 @@ pub fn shortcut(theme: &Theme, short: &str, long: &str) -> Widget<Shortcut> {
 }
 
 pub fn shortcuts(theme: &Theme, shortcuts: Vec<Widget<Shortcut>>) -> Widget<Shortcuts> {
-    let divider = label(&format!(" {} ", theme.icons.shortcutbar_divider))
+    let divider = components::label(&format!(" {} ", theme.icons.shortcutbar_divider))
         .foreground(theme.colors.shortcutbar_divider_fg);
     let shortcut_bar = Shortcuts::new(shortcuts, divider);
 
@@ -95,9 +63,9 @@ pub fn shortcuts(theme: &Theme, shortcuts: Vec<Widget<Shortcut>>) -> Widget<Shor
 }
 
 pub fn property(theme: &Theme, name: &str, value: &str) -> Widget<Property> {
-    let name = label(name).foreground(theme.colors.property_name_fg);
-    let divider = label(&format!(" {} ", theme.icons.property_divider));
-    let value = label(value).foreground(theme.colors.default_fg);
+    let name = components::label(name).foreground(theme.colors.property_name_fg);
+    let divider = components::label(&format!(" {} ", theme.icons.property_divider));
+    let value = components::label(value).foreground(theme.colors.default_fg);
 
     // TODO: Remove when size constraints are implemented
     let name_w = name.query(Attribute::Width).unwrap().unwrap_size();
@@ -117,32 +85,9 @@ pub fn property_list(_theme: &Theme, properties: Vec<Widget<Property>>) -> Widge
 }
 
 pub fn tabs(theme: &Theme, tabs: Vec<Widget<Label>>) -> Widget<Tabs> {
-    let line =
-        label(&theme.icons.tab_overline.to_string()).foreground(theme.colors.tabs_highlighted_fg);
+    let line = components::label(&theme.icons.tab_overline.to_string())
+        .foreground(theme.colors.tabs_highlighted_fg);
     let tabs = Tabs::new(tabs, line);
 
     Widget::new(tabs).height(2)
-}
-
-pub fn table(
-    theme: &Theme,
-    labels: &[&str],
-    widths: &[u16],
-    items: &[impl List],
-    profile: &Profile,
-) -> Widget<Table> {
-    let items = items.iter().map(|item| item.row(theme, profile)).collect();
-
-    let header = table_header(theme, labels, widths);
-    let table = Table::new(header);
-
-    let widths = AttrValue::Payload(PropPayload::Vec(
-        widths.iter().map(|w| PropValue::U16(*w)).collect(),
-    ));
-
-    Widget::new(table)
-        .content(AttrValue::Table(items))
-        .custom("widths", widths)
-        .background(theme.colors.labeled_container_bg)
-        .highlight(theme.colors.item_list_highlighted_bg)
 }
