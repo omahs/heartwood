bc1a2e9dc2e0788f0557877219b5f872913e6b0c
Erik Kundt <erik@zirkular.io>
2023-05-23 17:44:57 +0200

    tui: Fix global navigation subscriptions

--- radicle-tui/src/app/subscription.rs
+++ radicle-tui/src/app/subscription.rs
@@ -3,20 +3,6 @@ use std::hash::Hash;
 use tuirealm::event::{Key, KeyEvent, KeyModifiers};
 use tuirealm::{Sub, SubClause, SubEventClause};
 
-pub fn navigation<Id, UserEvent>() -> Vec<Sub<Id, UserEvent>>
-where
-    Id: Clone + Hash + Eq + PartialEq,
-    UserEvent: Clone + Eq + PartialEq + PartialOrd,
-{
-    vec![Sub::new(
-        SubEventClause::Keyboard(KeyEvent {
-            code: Key::Tab,
-            modifiers: KeyModifiers::NONE,
-        }),
-        SubClause::Always,
-    )]
-}
-
 pub fn global<Id, UserEvent>() -> Vec<Sub<Id, UserEvent>>
 where
     Id: Clone + Hash + Eq + PartialEq,
